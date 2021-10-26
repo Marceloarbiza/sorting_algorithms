4061dcf9bebba54fc5c6223fa3d07822f7dd1d3b
Marceloarbiza <3338@holbertonschool.com>
2021-10-25 17:53:20 -0700

    D

--- 100-shell_sort.c
+++ 100-shell_sort.c
@@ -7,30 +7,33 @@
 
 void shell_sort(int *array, size_t size)
 {
-	size_t i, j, gap;
+	size_t i, j, r, gap = 1;
 	int aux;
 
 	if (size < 2 || !array)
 		return;
 
 	while (gap < size)
-		gap = (gap * 3) + 1;
+		gap = gap * 3 + 1;
 
-	while (gap > 0)
+	while (gap > 1)
 	{
-		for (i = gap; i < size; i++)
-		{
-			aux = array[i];
-			j = i;
-			while ((j > gap - 1) && (array[j - gap] >= aux))
-			{
-				array[j] = array[j - gap];
-				j = j - gap;
-			}
-			array[j] = aux;
+		gap = (gap - 1) / 3;
 
+		for (i = 0; i < size - 1; i++)
+		{
+			r = i;
+			for (j = i + gap; j < size; j += gap)
+			{
+				if (array[i] < array[r])
+				{
+					r = j;
+				}
+			}
+			aux = array[r];
+			array[r] = array[i];
+			array[i] = aux;
 		}
 		print_array(array, size);
-		gap = gap / 3;
 	}
 }
